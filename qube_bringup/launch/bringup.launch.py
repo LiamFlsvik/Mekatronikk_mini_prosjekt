64f9f54f48337424cebfc442a24768e91ad6c3f9
Liam <liam.folsviks@gmail.com>
2025-04-02 09:10:00 +0200

    test

---
+++ qube_bringup/launch/bringup.launch.py
@@ -0,0 +1,93 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command
+from launch_ros.actions import Node
+from launch.actions import TimerAction
+import os
+from ament_index_python.packages import get_package_share_directory
+from time import sleep
+def generate_launch_description():
+    # Argumenter
+    device_arg = DeclareLaunchArgument("device", default_value="/dev/ttyACM0")
+    baud_rate_arg = DeclareLaunchArgument("baud_rate", default_value="115200")
+    simulation_arg = DeclareLaunchArgument("simulation", default_value="false")
+
+    # Konfigurasjoner
+    device = LaunchConfiguration("device")
+    baud_rate = LaunchConfiguration("baud_rate")
+    simulation = LaunchConfiguration("simulation")
+
+    # Sti til URDF
+    qube_bringup_dir = get_package_share_directory("qube_bringup")
+    urdf_file = os.path.join(qube_bringup_dir, "urdf", "controlled_qube.urdf.xacro")
+
+    # Dynamisk URDF med xacro-kommando
+    robot_description = Command([
+        "xacro ", urdf_file,
+        " device:=", device,
+        " baud_rate:=", baud_rate,
+        " simulation:=", simulation
+    ])
+
+    # Noder
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        output="screen",
+        parameters=[{"robot_description": robot_description}]
+    )
+
+    ros2_control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[{"robot_description": robot_description}],
+        output="screen"
+    )
+
+    controller_broadcaster_joint = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+        output="screen"
+    )
+
+    velocity_controller_node = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["velocity_controller", "--controller-manager", "/controller_manager"],
+        output="screen"
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+    )
+
+        
+    return LaunchDescription([
+        device_arg,
+        baud_rate_arg,
+        simulation_arg,
+
+        robot_state_publisher_node,
+
+        TimerAction(
+            period=2.0,
+            actions=[ros2_control_node]
+        ),
+
+        TimerAction(
+            period=3.0,
+            actions=[controller_broadcaster_joint]
+        ),
+
+        TimerAction(
+            period=4.0,
+            actions=[velocity_controller_node]
+        ),
+
+        rviz_node,
+    ])
--- qube_bringup/setup.py
+++ qube_bringup/setup.py
@@ -1,5 +1,5 @@
 from setuptools import find_packages, setup
-
+import os
 package_name = 'qube_bringup'
 
 setup(
@@ -10,6 +10,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), ['launch/bringup.launch.py']),
+        (os.path.join('share', package_name, 'urdf'), ['urdf/controlled_qube.urdf.xacro'])
     ],
     install_requires=['setuptools'],
     zip_safe=True,
